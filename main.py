c2013d6d49193044973471ab85e5858624b66b8b
3011 <choybb@qq.com>
2023-04-16 13:09:03 +0800

    Add custom api url.

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+"""===== Config ====="""
+bot_token = ""  # 机器人的token
+allowed_users = []  # 用户ID列表，建议个人使用
+message_update_time = 1.5  # seconds 消息更新间隔
+
+chatgpt_api_url = ""  # Optional 可选
+
+
+"""===== ChatGPT ====="""
+from revChatGPT.V1 import Chatbot
+
+bot = Chatbot(config={"access_token": ""}, base_url=chatgpt_api_url)
+
+
+def set_access_token(access_token):
+    bot.set_access_token(access_token)
+
+
+def reset_chat():
+    bot.reset_chat()
+
+
+def get_reply_stream(prompt):
+    prev_text = ""
+    for data in bot.ask(prompt):
+        prev_text = data["message"]
+        yield False, prev_text
+    yield True, prev_text
+
+
+"""===== Bot ====="""
 import time
 import logging
 from telegram import (
@@ -11,22 +42,14 @@ from telegram.ext import (
     filters,
 )
 from telegram.constants import ParseMode
-import chatgpt
-
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
-bot_token = ""  # 机器人的token
-allowed_users = [] # 用户ID列表，建议个人使用
-message_update_time = 1.5  # seconds 消息更新间隔
-
-need_set_access_token = True
-
 
 async def reset_reply(reply_message):
-    chatgpt.reset_chat()
+    reset_chat()
     await reply_message(text="An error occurred.\nConversation has been reset.")
 
 
@@ -39,7 +62,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(text="Please set access token first.")
         return
 
-    chatgpt.reset_chat()
+    reset_chat()
     await reset_reply(update.message.reply_text)
 
 
@@ -58,7 +81,7 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         prev_time = time.time()
         prev_reply_text = ""
-        for is_done, reply_text in chatgpt.get_reply_stream(message):
+        for is_done, reply_text in get_reply_stream(message):
             if is_done:
                 try:
                     if prev_reply_text != reply_text:
@@ -95,7 +118,7 @@ async def set_access_token(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     try:
         access_token = update.message.text.split(" ")[1]
-        chatgpt.set_access_token(access_token)
+        set_access_token(access_token)
         global need_set_access_token
         need_set_access_token = False
         await update.message.reply_text(text="Access token set.")
